df84cad58d364674f82dc7b1e3c886e045619520
matsuoka.yuki <matsuoka.yuki@intra.aws-axion.net>
2022-08-09 13:24:04 +0900

    create memodetailscreen

--- App.jsx
+++ App.jsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}/** propsを渡すことで呼び出す側から表示を制御できる */>
-
-      <AppBar />
-
-      <MemoList />
-
-      <CircleButton>+</CircleButton>
-
-    </View>
+    <MemoDetailScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, /** コンテナの設定を画面いっぱいに反映してくださいね。一番外側のviewに */
-    backgroundColor: '#F0F4F8', /** white */
-    /** alignItems: 'center',
-    justifyContent: 'flex-start', /** flex-start上にflex-end下に */
-  },
-});
---
+++ src/screens/MemoListScreen.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import AppBar from '../components/AppBar'; /** ../で一段階上の階層に移動する */
+import MemoList from '../components/MemoList';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoListScreen() {
+  return (
+    <View style={styles.container}/** propsを渡すことで呼び出す側から表示を制御できる */>
+      <AppBar />
+      <MemoList />
+      <CircleButton style={{ top: 24 }}>+</CircleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, /** コンテナの設定を画面いっぱいに反映してくださいね。一番外側のviewに */
+    backgroundColor: ' #F0F4F8', /** 白include薄黄色 */
+  },
+});
